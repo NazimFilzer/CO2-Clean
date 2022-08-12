e2f41f25ea5e199bf95a0f1d565a745de615bffc
Nazim Filzer <nazimfilzer@gmail.com>
2022-08-12 09:08:49 +0530

    Basic api call

--- routes/apiCall.js
+++ routes/apiCall.js
@@ -8,13 +8,48 @@ require("dotenv").config();
 
 
 
-router.get("/calculate",(req,res)=>{
+router.get("/calculate", (req, res) => {
     res.render("calculate")
 })
 
 
-router.post("/calculate",(req,res)=>{
-    
+router.post("/calculate", async(req, res) => {
+    const query = req.body.material;
+    const amount = req.body.weight;
+    const unitType = req.body.unit;
+
+    try {
+        const url = "https://beta3.api.climatiq.io/search?query=" + query;
+        console.log(url);
+
+
+        const response = await fetch(url, {
+            method: "GET",
+            headers: {
+                Authorization: "Bearer " + process.env.API_KEY,
+            },
+
+        });
+        const data = await response.json();
+        const output = data.results[1]
+        console.log(output);
+
+
+        const emissionValue = output.factor * amount;
+        co2Value = { value: emissionValue }
+        console.log(co2Value);
+        const name = output.name;
+
+        // const saveing = new Emission({
+        //     material: name
+        // })
+        // saveing.save()
+        res.render("calculate", { output, co2Value })
+
+    } catch (error) {
+        console.log(error);
+    }
+
 })
 
 
